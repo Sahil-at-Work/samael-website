85359c3aaec257efee5a6e85e5eaf04326526abc
Sahil-at-Work <sahil.asawant30@gmail.com>
2025-08-31 10:08:49 +0530

    i

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -7,17 +7,17 @@ const AboutSection = () => {
       title="About Me"
       subtitle="Electronics & Telecommunication Engineering Student | Astronomy Enthusiast | Cybersecurity Advocate"
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-        <div className="relative w-full flex justify-center">
-          <div className="aspect-square w-full max-w-md rounded-half border-8 border-yellow-500 overflow-hidden shadow-xl relative">
-            <img
-              src="https://media.licdn.com/dms/image/v2/D5622AQFmQnoRRdm2vA/feedshare-shrink_1280/feedshare-shrink_1280/0/1690176262751?e=1759363200&v=beta&t=wPADsRrTktvPdA8hRMRo4uT1CSO0Hqhd3lxGQuLL4G0"
-              alt="Person coding on laptop"
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div className="absolute -bottom-6 -right-6 w-48 h-48 bg-blue-500 rounded-full opacity-20 z-0"></div>
-        </div>
+<div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+  <div className="relative w-full flex justify-center">
+    <div className="w-full max-w-md h-[400px] overflow-hidden border-[5px] border-yellow-500 rounded-[20px] shadow-xl">
+      <img
+        src="https://media.licdn.com/dms/image/v2/D5622AQFmQnoRRdm2vA/feedshare-shrink_1280/feedshare-shrink_1280/0/1690176262751?e=1759363200&v=beta&t=wPADsRrTktvPdA8hRMRo4uT1CSO0Hqhd3lxGQuLL4G0"
+        alt="Person coding on laptop"
+        className="w-full object-cover object-top h-full"
+      />
+    </div>
+  </div>
+
 
         <div className="space-y-6">
           <p className="text-lg text-gray-600 dark:text-gray-300 leading-relaxed">
